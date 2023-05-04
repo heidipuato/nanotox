573789f09829750a846df7a9a0dd89c16a5a2bec
heidipuato <heidipuato@gmail.com>
2023-05-04 21:06:17 +0800

    settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
-    os.path.join(BASE_DIR,'static_root'),
+    os.path.join(BASE_DIR,'staticfiles'),
 ]
 
 # Default primary key field type
